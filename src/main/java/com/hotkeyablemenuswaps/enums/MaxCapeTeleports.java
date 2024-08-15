75aef7d9fa0412a3dfd465cbcef2d9d73055ac95
Viktor <viktor.horsmanheimo@gmail.com>
2024-08-15 23:36:00 +0300

    Add max cape features

--- src/main/java/com/hotkeyablemenuswaps/enums/MaxCapeTeleports.java
+++ src/main/java/com/hotkeyablemenuswaps/enums/MaxCapeTeleports.java
@@ -10,27 +10,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum MaxCapeTeleports {
-    WARRIORS_GUILD("Warrior's Guild", ComponentID.ADVENTURE_LOG_OPTIONS, 0),
-    FISHING_GUILD("Fishing Guild", ComponentID.ADVENTURE_LOG_OPTIONS, 1),
-    CRAFTING_GUILD("Crafting Guild", ComponentID.ADVENTURE_LOG_OPTIONS, 2),
-    FARMING_GUILD("Farming Guild", ComponentID.ADVENTURE_LOG_OPTIONS, 3),
-    OTTOS_GROTTO("Otto's Grotto", ComponentID.ADVENTURE_LOG_OPTIONS, 4),
-    RED_CHINS("Red Chinchompas", ComponentID.ADVENTURE_LOG_OPTIONS, 5, 1),
-    BLACK_CHINS("Black Chinchompas", ComponentID.ADVENTURE_LOG_OPTIONS, 5, 2),
-    HUNTER_GUILD("Hunter Guild", ComponentID.ADVENTURE_LOG_OPTIONS, 5, 3),
-    HOME("Home", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 0),
-    RIMMINGTON("Rimmington", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 1),
-    TAVERLEY("Taverley", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 2),
-    POLLNIVNEACH("Pollnivneach", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 3),
-    HOSIDIUS("Hosidius", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 4),
-    RELLEKA("Rellekka", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 5),
-    BRIMHAVEN("Brimhaven", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 6),
-    YANILLE("Yanille", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 7),
-    PRIFDDINAS("Prifddinas", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 8),
+    // Teleports
+    WARRIORS_GUILD("Warrior's Guild", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 0),
+    FISHING_GUILD("Fishing Guild", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 1),
+    CRAFTING_GUILD("Crafting Guild", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 2),
+    FARMING_GUILD("Farming Guild", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 3),
+    OTTOS_GROTTO("Otto's Grotto", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 4),
+    RED_CHINS("Red Chinchompas", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 5, 1),
+    BLACK_CHINS("Black Chinchompas", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 5, 2),
+    HUNTER_GUILD("Hunter Guild", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 5, 3),
+    HOME("Home", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 0),
+    RIMMINGTON("Rimmington", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 1),
+    TAVERLEY("Taverley", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 2),
+    POLLNIVNEACH("Pollnivneach", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 3),
+    HOSIDIUS("Hosidius", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 4),
+    RELLEKA("Rellekka", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 5),
+    BRIMHAVEN("Brimhaven", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 6),
+    YANILLE("Yanille", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 7),
+    PRIFDDINAS("Prifddinas", "Teleports", ComponentID.ADVENTURE_LOG_OPTIONS, 6, 8),
+
+    // Features
+    PESTLE_AND_MORTAR("Pestle and Mortar", "Features", ComponentID.DIALOG_OPTION_OPTIONS, 1, 1),
+    MITH_GRAPPLE("Mithril Grapple & Cross", "Features", ComponentID.DIALOG_OPTION_OPTIONS, 1, 2),
+    SPELLBOOK("Spellbook", "Features", ComponentID.DIALOG_OPTION_OPTIONS, 4),
+    STAMINA_BOOST("Stamina Boost", "Features", ComponentID.DIALOG_OPTION_OPTIONS, 5),
     ;
 
     static Map<String, MaxCapeTeleports> mapper;
     final String name;
+
+    @Getter
+    final String option;
 
     @Getter
     final int id;
@@ -46,9 +56,10 @@ public enum MaxCapeTeleports {
         mapper = Collections.unmodifiableMap(map);
     }
 
-    MaxCapeTeleports(String name, int id, int ...ops)
+    MaxCapeTeleports(String name, String option, int id, int ...ops)
     {
         this.name = name;
+        this.option = option;
         this.id = id;
         this.ops = new int[ops.length];
         System.arraycopy(ops, 0, this.ops, 0, ops.length);
